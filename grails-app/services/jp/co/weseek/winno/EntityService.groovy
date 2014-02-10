65f4d799b0bfe73cb4bafa2b62a3b6ae0db462f7
Yuki Takei <yuki@weseek.co.jp>
2014-02-10 22:39:32 +0900

    add EntityService and toggleAutoTimestamp method

---
+++ grails-app/services/jp/co/weseek/winno/EntityService.groovy
@@ -0,0 +1,36 @@
+package jp.co.weseek.winno
+
+import org.grails.datastore.gorm.events.AutoTimestampEventListener
+
+/**
+ * Service for entities
+ *
+ * @author Yuki Takei <yuki@weseek.co.jp>
+ *
+ */
+class EntityService {
+
+
+	/**
+	 * enable or disable 'autoTimestamp' of specified entity
+	 * @param entityClass
+	 * @param enabled
+	 */
+	void toggleAutoTimestamp(Class entityClass, boolean enabled) {
+		// scan all application listeners and find the AutoTimestampEventListener instance
+		AutoTimestampEventListener autoTseListener
+		for (def listener in grailsApplication.mainContext.getApplicationListeners()) {
+			if (listener instanceof AutoTimestampEventListener) {
+				autoTseListener = listener
+				break
+			}
+		}
+
+		// get PersistentEntity
+		def entity = autoTseListener.datastore.getMappingContext().getPersistentEntity(entityClass.name)
+
+		// change
+		autoTseListener.entitiesWithLastUpdated.put(entity, enabled)
+	}
+
+}
